049c1def52552a141dd814788e78b5ea97aa6856
syairaarif <putrisyafira4444@gmail.com>
2025-11-21 10:22:39 +0700

    membuat member class yang ]memiliki modifier public

--- app/src/main/java/com/example/quest6_194/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_194/viewmodel/SiswaViewModel.kt
@@ -2,4 +2,7 @@ package com.example.quest6_194.viewmodel
 
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow( value = Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
 }
