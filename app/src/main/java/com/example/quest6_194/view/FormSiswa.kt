c57e9b4eb47ae2b78036338ab4bfe700c2dd19d8
syairaarif <putrisyafira4444@gmail.com>
2025-11-21 10:34:38 +0700

    membuat variabel yang di butuhkan untuk inputan

--- app/src/main/java/com/example/quest6_194/view/FormSiswa.kt
+++ app/src/main/java/com/example/quest6_194/view/FormSiswa.kt
@@ -17,6 +17,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,4 +36,9 @@ fun FormSiswa (
     OnSubmitButtonClicked : (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ){
+
+    var txtNama by rememberSaveable { mutableStateOf(value = "")}
+    var txtAlamat by remember { mutableStateOf(value = "")}
+    var txtGender by remember { mutableStateOf(value = "")}
+    val listData: MutableList<String> = mutableListOf(txtNama, txtAlamat, txtGender)
 }
