b0e99797b7ec03ae2b5933689bf1116026a89b9b
syairaarif <putrisyafira4444@gmail.com>
2025-11-21 10:29:59 +0700

    membuat fungsi untuk tampilan form mahasiswa yang memiliki 2 paramter

---
+++ app/src/main/java/com/example/quest6_194/view/FormSiswa.kt
@@ -0,0 +1,34 @@
+package com.example.quest6_194.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa (
+    pilihanJK: List<String>,
+    OnSubmitButtonClicked : (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
+}
