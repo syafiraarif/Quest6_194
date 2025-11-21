bf3a482aab5946b970048447460a98ec5263d204
syairaarif <putrisyafira4444@gmail.com>
2025-11-21 10:56:43 +0700

    membuat komponen untuk garis batas

--- app/src/main/java/com/example/quest6_194/view/FormSiswa.kt
+++ app/src/main/java/com/example/quest6_194/view/FormSiswa.kt
@@ -97,6 +97,14 @@ fun FormSiswa (
                 }
             }
 
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
         }
     }
 }
